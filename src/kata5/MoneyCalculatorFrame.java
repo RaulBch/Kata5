8749a3e09aa15c7886369cd8c0130f99c6015f8a
Raul <>
2015-01-19 03:45:29 +0000

    Kata5

---
+++ src/kata5/MoneyCalculatorFrame.java
@@ -0,0 +1,66 @@
+package kata5;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class MoneyCalculatorFrame extends JFrame {
+
+    public MoneyCalculatorFrame() {
+        setTitle("Calculador de Divisas");
+        setMinimumSize(new Dimension(450, 150));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        createComponents();
+        setVisible(true);
+    }
+
+    private void createComponents() {
+        add(createExchangeDialog());
+        add(createToolbar(), BorderLayout.SOUTH);
+    }
+
+    private Component createExchangeDialog() {
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.add(new MoneyDialog());
+        panel.add(new CurrencyDialog());
+        return panel;
+    }
+
+    private Component createToolbar() {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        panel.add(createCalculateButton());
+        panel.add(createCancelButton());
+        return panel;
+    }
+
+    private JButton createCalculateButton() {
+        JButton calculate = new JButton("calculate");
+        calculate.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("calculating...");
+            }
+        });
+        return calculate;
+    }
+
+    private JButton createCancelButton() {
+        JButton cancel = new JButton("cancel");
+        cancel.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MoneyCalculatorFrame.this.dispose();
+            }
+        });
+        return cancel;
+    }
+    
+}
